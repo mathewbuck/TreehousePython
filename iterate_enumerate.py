32f107c85868651d7264c585e4e9658fcec73f10
Loafz <mathewbuck38@gmail.com>
2021-04-30 20:08:30 -0700

    Add files via upload

---
+++ iterate_enumerate.py
@@ -0,0 +1,6 @@
+groceries = ['candy', 'hot pockets', 'kung fu movie', 'peanut butter', 'bread', 'cat food', 'wine']
+
+
+for index, item in enumerate(groceries, 1):
+    print(f'{index}. {item}')
+ 
